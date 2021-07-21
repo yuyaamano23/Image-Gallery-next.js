a1b28ec08f242ae7af8f7699c722487bb117dcb2
yuyaamano23 <nba.chako.nba@icloud.com>
2021-07-21 23:29:28 +0900

    ログイン状態によってヘッダー表示切り替え

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,29 @@
 import React, { FC } from 'react';
 import styles from 'styles/components/Header.module.scss';
+import { useAuthentication } from 'hooks/authentication';
+import Logout from 'components/logout';
+import Link from 'next/link';
 
 const Header: FC = () => {
+    const { user } = useAuthentication();
     return (
         <div>
-            <div className="bg-red-500">ここに共通ヘッダーを書いていくンゴ</div>
+            <div className="bg-red-500">
+                <button>
+                    <Link href="/">
+                        <a>投稿一覧ページへ</a>
+                    </Link>
+                </button>
+                <div>
+                    {user ? (
+                        <Logout />
+                    ) : (
+                        <Link href="/login" passHref>
+                            <button>ログインする</button>
+                        </Link>
+                    )}
+                </div>
+            </div>
             <div className={styles.test}>Header.module.scssの確認</div>
         </div>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Logout from 'components/logout';
-import { useAuthentication } from 'hooks/authentication';
-import Link from 'next/link';
 
 export default function Home() {
-    const { user } = useAuthentication();
     return (
         <div>
-            <div>投稿一覧ページ</div>
-            <div>
-                {user ? (
-                    <Logout />
-                ) : (
-                    <div>
-                        <Link href="/login" passHref>
-                            <button>ログインする</button>
-                        </Link>
-                        <button className="w-24 m-4 p-4 rounded-sm bg-yellow-500 text-center text-white">
-                            test
-                        </button>
-                    </div>
-                )}
-            </div>
+            <div>投稿一覧ページです</div>
         </div>
     );
 }
