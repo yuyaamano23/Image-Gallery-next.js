ffbbdffb084d4dad3c95c5fe2a2e30b9e343f1ee
yuyaamano23 <nba.chako.nba@icloud.com>
2021-07-22 00:08:52 +0900

    画像投稿ボタンを表示でけた

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import Uploader from 'components/uploader';
 
 export default function Home() {
     return (
         <div>
             <div>投稿一覧ページです</div>
+            <Uploader />
         </div>
     );
 }
