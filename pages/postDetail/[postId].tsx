f5040cef6a66de395ad3be5ef1320b616f271178
yuyaamano23 <nba.chako.nba@icloud.com>
2021-07-26 20:23:47 +0900

    画像詳細ページでけた

--- pages/postDetail/[postId].tsx
+++ pages/postDetail/[postId].tsx
@@ -1,42 +1,71 @@
 import React, { FC, useEffect, useState } from 'react';
-import { User } from 'models/User';
+import { Post } from 'models/Post';
 import { useRouter } from 'next/router';
 import firebase from 'firebase/app';
+import styles from 'styles/components/postsIndex.module.scss';
+import Image from 'next/image';
 
 type Query = {
     postId: string;
 };
 
 const PostDetail: FC = () => {
-    // const [user, setUser] = useState<User>(null);
+    const [post, setPost] = useState<Post>(null);
     const router = useRouter();
     const query = router.query as Query;
 
-    //     useEffect(() => {
-    //         // 初回レンダリングを考慮するために query に値がある場合だけ処理するように調整します。
-    //         if (query.uid === undefined) {
-    //             return;
-    //         }
+    useEffect(() => {
+        // 初回レンダリングを考慮するために query に値がある場合だけ処理するように調整します。
+        if (query.postId === undefined) {
+            return;
+        }
 
-    //         async function loadUser() {
-    //             const doc = await firebase
-    //                 .firestore()
-    //                 .collection('users')
-    //                 .doc(query.uid)
-    //                 .get();
+        async function loadPost() {
+            const doc = await firebase
+                .firestore()
+                .collection('posts')
+                .doc(query.postId)
+                .get();
 
-    //             if (!doc.exists) {
-    //                 return;
-    //             }
+            if (!doc.exists) {
+                return;
+            }
 
-    //             const gotUser = doc.data() as User;
-    //             gotUser.uid = doc.id;
-    //             setUser(gotUser);
-    //         }
-    //         // useEffectはasyncが使えないから関数を分けている;
-    //         loadUser();
-    //     }, [query.uid]);
+            const gotPost = doc.data() as Post;
+            gotPost.id = doc.id;
+            gotPost.createdAt = new Date(gotPost.createdAt.seconds * 1000);
+            setPost(gotPost);
+        }
+        // useEffectはasyncが使えないから関数を分けている;
+        loadPost();
+    }, [query.postId]);
 
-    return <div>{query ? query.postId : 'ロード中...'}のページ</div>;
+    return (
+        <div>
+            {post ? (
+                <div className={styles.card}>
+                    <Image
+                        src={`${post.downloadUrl}`}
+                        // この数字を大きくする分には比率は崩れなさそう
+                        width={1000}
+                        height={1000}
+                        objectFit="contain"
+                        alt={`${post.title}`}
+                        className={styles.img}
+                    />
+                    <div className={styles.container}>
+                        <h4>
+                            <b>{post.title}</b>
+                        </h4>
+                        <p>
+                            {post.createdAt.toLocaleString('ja-JP').toString()}
+                        </p>
+                    </div>
+                </div>
+            ) : (
+                'ロード中...'
+            )}
+        </div>
+    );
 };
 export default PostDetail;
