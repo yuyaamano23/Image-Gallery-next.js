10aa668c49c1a6698df86f58b79a2dbf937d16d9
yuyaamano23 <nba.chako.nba@icloud.com>
2021-08-04 00:08:34 +0900

    sidenav設置できた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import '../lib/firebase';
 import { RecoilRoot } from 'recoil';
 import Header from 'components/Header';
 import { ChakraProvider } from '@chakra-ui/react';
+import Sidebar from 'components/sidebar/sidebar';
 
 function MyApp({ Component, pageProps }) {
     return (
         <RecoilRoot>
             <ChakraProvider>
                 <Header />
+                <Sidebar />
                 <Component {...pageProps} />
             </ChakraProvider>
         </RecoilRoot>
