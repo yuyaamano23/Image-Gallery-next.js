8cfe009cf06780de15762ead578c55c2d2d84b1f
yuyaamano23 <nba.chako.nba@icloud.com>
2021-08-10 20:54:04 +0900

    投稿一覧のレイアウト横並びにできた

--- components/postsIndex.tsx
+++ components/postsIndex.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { Post } from 'models/Post';
 import styles from 'styles/components/postsIndex.module.scss';
 import Link from 'next/link';
-import { Button, Spinner } from '@chakra-ui/react';
+import { Button, Flex, Spinner } from '@chakra-ui/react';
 import { RepeatIcon } from '@chakra-ui/icons';
 import LikeButton from './likeButton';
 
@@ -67,66 +67,72 @@ const PostsIndex: FC = () => {
                 <RepeatIcon w={6} h={6} color="red.500" />
             </Button>
 
-            {posts ? (
-                <>
-                    {posts.map((post) => {
-                        const parsedCreatedAt = new Date(
-                            post.createdAt.seconds * 1000
-                        );
-                        return (
-                            <div className={styles.link} key={post.id}>
-                                <div className={styles.card}>
-                                    <Link
-                                        href={`/postDetail/${post.id}`}
-                                        passHref
-                                    >
-                                        <a>
-                                            <Image
-                                                src={`${post.downloadUrl}`}
-                                                // この数字を大きくする分には比率は崩れなさそう
-                                                width={1000}
-                                                height={1000}
-                                                objectFit="contain"
-                                                alt={`${post.title}`}
-                                                className={styles.img}
-                                            />
-                                        </a>
-                                    </Link>
-                                    <div className={styles.container}>
+            <div className={styles.flex}>
+                {posts ? (
+                    <>
+                        {posts.map((post) => {
+                            const parsedCreatedAt = new Date(
+                                post.createdAt.seconds * 1000
+                            );
+                            return (
+                                <div className={styles.link} key={post.id}>
+                                    <div className={styles.card}>
                                         <Link
                                             href={`/postDetail/${post.id}`}
                                             passHref
                                         >
                                             <a>
-                                                <h4>
-                                                    <b>{post.title}</b>
-                                                </h4>
-                                                <p>
-                                                    {parsedCreatedAt
-                                                        .toLocaleString('ja-JP')
-                                                        .toString()}
-                                                </p>
-                                                <p>投稿者:{post.authorName}</p>
+                                                <Image
+                                                    src={`${post.downloadUrl}`}
+                                                    // この数字を大きくする分には比率は崩れなさそう
+                                                    width={1000}
+                                                    height={1000}
+                                                    objectFit="contain"
+                                                    alt={`${post.title}`}
+                                                    className={styles.img}
+                                                />
                                             </a>
                                         </Link>
-                                        <LikeButton postId={post.id} />
+                                        <div className={styles.container}>
+                                            <Link
+                                                href={`/postDetail/${post.id}`}
+                                                passHref
+                                            >
+                                                <a>
+                                                    <h4>
+                                                        <b>{post.title}</b>
+                                                    </h4>
+                                                    <p>
+                                                        {parsedCreatedAt
+                                                            .toLocaleString(
+                                                                'ja-JP'
+                                                            )
+                                                            .toString()}
+                                                    </p>
+                                                    <p>
+                                                        投稿者:{post.authorName}
+                                                    </p>
+                                                </a>
+                                            </Link>
+                                            <LikeButton postId={post.id} />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        );
-                    })}
-                </>
-            ) : (
-                <>
-                    <Spinner
-                        thickness="4px"
-                        speed="0.65s"
-                        emptyColor="gray.200"
-                        color="blue.500"
-                        size="xl"
-                    />
-                </>
-            )}
+                            );
+                        })}
+                    </>
+                ) : (
+                    <>
+                        <Spinner
+                            thickness="4px"
+                            speed="0.65s"
+                            emptyColor="gray.200"
+                            color="blue.500"
+                            size="xl"
+                        />
+                    </>
+                )}
+            </div>
         </React.Fragment>
     );
 };
